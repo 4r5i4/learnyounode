a8276d6867a585c1cf52a027d9333542c48cbc34
ArsiaArdalan <parthiardalan@gmail.com>
2018-03-21 19:39:42 -0400

    added a 1return`:  to make sure once error is encountered, the function exits

--- filteredLs.js
+++ filteredLs.js
@@ -5,7 +5,7 @@ var directory =  process.argv[2];
 var list = [];
 fs.readdir(directory, function (err, files){
     if(err) {
-        console.log(err)  
+        return console.log(err)  
     } 
     files.forEach(file => {
         // console.log('file::', file);
