66c0c0b080d55a0684e106394b20a7d9ed78d465
ArsiaArdalan <parthiardalan@gmail.com>
2018-03-21 21:43:27 -0400

    with official solution

--- httpclient.js
+++ httpclient.js
@@ -7,3 +7,18 @@ http.get(URL, function(response){
         
     })
 })
+
+
+//official solution
+
+/**
+ * 
+   var http = require('http')
+
+   http.get(process.argv[2], function (response) {
+     response.setEncoding('utf8')
+     response.on('data', console.log)
+     response.on('error', console.error)
+   }).on('error', console.error)
+
+ */
